d3a9b561a2d33e3c3c77fbe8981982ba96cc1e81
Dm.Derbin <dds9435@gmail.com>
2016-09-24 12:31:48 +0300

    upd players module

--- Assets/Scripts/players.gd
+++ Assets/Scripts/players.gd
@@ -1,7 +1,20 @@
 extends Node
 
-var playerList = {"Timofffee" : {
-						"ID":0, 
-						"national":"Poland",
-						"type_player":"human"}
-				}
+var playerList = {}
+#var playerList = {"Timofffee" : {
+#						"ID":0, 
+#						"national":"Random",
+#						"type_player":"human"}
+#				  "AI1" : {
+#						"ID":1, 
+#						"national":"Random",
+#						"type_player":"ai"}
+#				}
+				
+func _ready():
+	add_player('Timofffee', 'human', 'Poland')
+	add_player('ai1', 'ai', 'Poland')
+	print (playerList)
+	
+func add_player(name, type, national):
+	playerList[name] = {'ID':playerList.size(), "national":national, 'type_player':type}
--- Assets/Scripts/unitManager.gd
+++ Assets/Scripts/unitManager.gd
@@ -1,6 +1,6 @@
 extends Node2D
 var unit
-onready var player_list = get_node("/root/players").playerList
+onready var players = get_node("../../Players")
 var unit_scene = preload("res://Assets/Scenes/unit.tscn")
 	
 func _ready(): 
@@ -8,6 +8,6 @@ func _ready():
 
 func add_unit(player_name, unit_type, unit_cord):
 	unit = unit_scene.instance()
-	var unit_class = unit.Unit.new(player_list[player_name], unit_type, unit_cord) 
+	var unit_class = unit.Unit.new(players.playerList[player_name], unit_type, unit_cord) 
 	unit.set_pos(unit_class.cord)
 	add_child(unit)
