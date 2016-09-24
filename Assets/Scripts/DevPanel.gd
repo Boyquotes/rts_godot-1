953de264924ff40669426430f690f33fbc610771
Timofffee <krougz@live.ru>
2016-09-24 18:38:02 +0400

    Unit update #4 Added generation of list of units in DevTools

--- Assets/Scripts/DevPanel.gd
+++ Assets/Scripts/DevPanel.gd
@@ -4,8 +4,32 @@ var current_unit = null
 
 onready var cam = get_node("../Camera2D")
 onready var u_inst = get_node("../terra/units")
+onready var unit_panel = get_node("unit_manager/national_tabs")
+var unit_list 
 
 func _ready():
+	unit_list = load("res://Assets/Configs/unit_list.gd").new().unit
+	for national in unit_list.keys():
+		var national_tab = Tabs.new()
+		national_tab.set_name(national)
+		unit_panel.add_child(national_tab)
+		var vbc = VBoxContainer.new()
+		vbc.set_name("units")
+		unit_panel.get_node(national).add_child(vbc)
+		var label = Label.new()
+		label.set_name("infantry")
+		label.set_text("infantry")
+		unit_panel.get_node(national+"/units").add_child(label)
+		var grid = GridContainer.new()
+		grid.set_name("infantry_"+"grid")
+		unit_panel.get_node(national+"/units").add_child(grid)
+		for unit in unit_list[national]:
+			var button = Button.new()
+			button.set_name(unit)
+			button.set_button_icon(load("res://Assets/Textures/Units/"+unit+".png"))
+			button.connect("pressed", self, "_on_unit_select", [unit])
+			unit_panel.get_node(national+"/units/"+grid.get_name()).add_child(button)
+			 
 	set_fixed_process(true)
 
 func _fixed_process(delta):
