990321912b66aaa42d59cb61effd437568b646d7
Dm.Derbin <dds9435@gmail.com>
2016-09-27 13:03:37 +0300

    upd targetPanel

--- Assets/Scripts/targetManager.gd
+++ Assets/Scripts/targetManager.gd
@@ -5,26 +5,32 @@ extends Control
 # var b = "textvar"
 export var font_color = Color(0,0,0)
 onready var u_inst = get_node("../../terra/units")
-onready var prop = get_node("target_manager/ScrollContainer/VBoxContainer")
+onready var prop = get_node("target_manager/ScrollContainer/VBoxContainer/GridContainer")
 func _ready():
 	set_process(true)
 var i = 0
 func _process(delta):
-	print (u_inst.unit_select_group)
+	#print (u_inst.unit_select_group)
 	if u_inst.unit_select_group != []:
 		show()
 		if i == 0:
 			var d = {}
 			for unit in u_inst.unit_select_group:
-				if d.has(unit.unit_class.type):
-					d[unit.unit_class.type] = d[unit.unit_class.type] + 1
+				if d.has(unit.unit_class.unit):
+					d[unit.unit_class.unit] = d[unit.unit_class.unit] + 1
 				else:
-					d[unit.unit_class.type] = 1
+					d[unit.unit_class.unit] = 1
 			for key in d:
-				var label = Label.new()
-				label.set_text(' ' + key + ' ' + str(d[key]))
-				label.add_color_override("font_color", font_color)
-				prop.add_child(label)
+				var button = Button.new()
+				button.set_name(key)
+				var path_file_icon = "res://Assets/Textures/Units/" + key + ".png"
+				if File.new().file_exists(path_file_icon) != true:
+					path_file_icon = "res://Assets/Textures/Units/default.png"
+				button.set_button_icon(load(path_file_icon))
+				button.set_text(str(d[key]))
+				button.set_flat(true)
+				button.add_color_override("font_color", font_color)
+				prop.add_child(button)
 		i += 1
 	else:
 		for unit in prop.get_children():
