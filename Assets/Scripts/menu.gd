025b665b7a8288caf30ae84de2f1533114519afa
Dm.Derbin <dds9435@gmail.com>
2016-10-18 17:14:22 +0300

    fix menu

--- Assets/Scripts/menu.gd
+++ Assets/Scripts/menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 const aspect_ratio_4x3 = 4.0/3.0
 const aspect_ratio_16x9 = 16.0/9.0
@@ -8,12 +8,13 @@ onready var tf = get_node('ParallaxBackground/TextureFrame')
 onready var ui_player = get_node("UI_player")
 
 func _ready():
-	var screen_size = OS.get_window_size() 
-	var r = screen_size.x / screen_size.y 
-	print(screen_size, ' ', r)
-	if r == aspect_ratio_4x3:		tf.set_texture(load(path_splash + '1_4x3.jpg'))
-	elif r == aspect_ratio_5x4:		tf.set_texture(load(path_splash + '1_5x4.jpg'))
-	else:							tf.set_texture(load(path_splash + '1_16x9.jpg'))
+	pass
+#	var screen_size = OS.get_window_size() 
+#	var r = screen_size.x / screen_size.y 
+#	print(screen_size, ' ', r)
+#	if r == aspect_ratio_4x3:		tf.set_texture(load(path_splash + '1_4x3.jpg'))
+#	elif r == aspect_ratio_5x4:		tf.set_texture(load(path_splash + '1_5x4.jpg'))
+#	else:							tf.set_texture(load(path_splash + '1_16x9.jpg'))
 
 func _on_bt_exit_pressed():
 	get_tree().quit()
